1bb15caaf3bcdc5af49ff629010b59c5956559b6
maeta187 <nexus18789@gmail.com>
2025-04-22 22:00:01 +0900

    ✨: ログイン機能のアクション実装

---
+++ src/app/login/actions.ts
@@ -0,0 +1,32 @@
+'use server'
+import { createClient } from '@/lib/supabaseServerClient'
+import type { LoginFormType } from '@/types'
+
+export const login = async (input: LoginFormType) => {
+	try {
+		const supabase = await createClient()
+
+		const { error } = await supabase.auth.signInWithPassword(input)
+		if (error) {
+			return {
+				success: false,
+				message: error?.message || 'ログインに失敗しました',
+				status: error?.status || 500,
+				error: error
+			}
+		}
+
+		return {
+			success: true,
+			message: 'ログインしました'
+		}
+	} catch (error) {
+		return {
+			success: false,
+			message:
+				error instanceof Error ? error.message : '予期せぬエラーが発生しました',
+			status: 500,
+			error
+		}
+	}
+}
