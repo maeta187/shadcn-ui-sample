3b20939758cbe75a69b859fc045779dc8f9e779d
maeta187 <nexus18789@gmail.com>
2025-04-22 22:01:18 +0900

    ✨: ログインページの作成

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import { createClient } from '@/lib/supabaseServerClient'
+import { redirect } from 'next/navigation'
+import { LoginForm } from './_components/LoginForm'
+
+export default async function Page() {
+	const supabase = await createClient()
+	const {
+		data: { user }
+	} = await supabase.auth.getUser()
+
+	if (user) {
+		redirect('/')
+	}
+
+	return (
+		<div className='flex min-h-[calc(100vh-64px-64px)] items-center justify-center'>
+			<LoginForm />
+		</div>
+	)
+}
