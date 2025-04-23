d2c08f68f4fcdf6537eff2bbea181b0e805fbca1
maeta187 <nexus18789@gmail.com>
2025-04-23 21:27:40 +0900

    ✨ : LoginFormの実装

---
+++ src/app/login/_components/LoginForm.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { InputForm } from '@/app/login/_components/InputForm'
+import { login } from '@/app/login/actions'
+import { LoginFormSchema } from '@/schemas'
+import type { LoginFormType } from '@/types'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+const defaultValues = {
+	email: '',
+	password: ''
+}
+
+export const LoginForm = () => {
+	const router = useRouter()
+
+	const form = useForm<LoginFormType>({
+		resolver: zodResolver(LoginFormSchema),
+		defaultValues
+	})
+
+	const onSubmit = async (data: LoginFormType) => {
+		await new Promise(async (resolve) => {
+			try {
+				const res = await login(data)
+				if (!res?.success) {
+					toast.error(res?.message || 'ログインに失敗しました')
+					return
+				}
+				resolve(res.success)
+				toast.success(res.message)
+				router.push('/')
+				router.refresh()
+			} catch (error) {
+				if (error instanceof Error) {
+					toast.error(error.message)
+				} else {
+					toast.error('アカウント登録に失敗しました')
+				}
+			}
+		})
+	}
+
+	return (
+		<div className='mx-auto w-2xl rounded-lg bg-white p-6 shadow-2xl'>
+			<h2 className='text-2xl font-bold'>ログイン</h2>
+			<InputForm form={form} onSubmit={onSubmit} />
+		</div>
+	)
+}
