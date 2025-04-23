82a230a2ccfad2813f2b38c7c2ab814be9b7296f
maeta187 <nexus18789@gmail.com>
2025-04-23 21:29:19 +0900

    🎨 : ナビゲーション-Loginの追加

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,44 +5,64 @@ import {
 	NavigationMenuItem,
 	NavigationMenuList
 } from '@/components/ui/navigation-menu'
-import { User } from '@supabase/supabase-js'
+import { Session } from '@supabase/supabase-js'
 import Link from 'next/link'
 
 interface NavigationProps {
-	user: User | null
+	session: Session | null
 }
 
-const navigationList: { title: string; href: string }[] = [
-	{
-		title: 'Signup',
-		href: '/signup'
-	}
-]
-
-export const Header = ({ user }: NavigationProps) => {
-	console.log(user)
-
+export const Header = ({ session }: NavigationProps) => {
 	return (
 		<header className='flex h-16 items-center border-b px-6'>
 			<Button variant='ghost' asChild>
 				<Link href='/'>header</Link>
 			</Button>
-			<HeaderNavigationMenu />
+			<HeaderNavigationMenu session={session} />
 		</header>
 	)
 }
 
-const HeaderNavigationMenu = () => {
+const HeaderNavigationMenu = ({ session }: NavigationProps) => {
+	const navigationList: {
+		title: string
+		href: string
+		className: string
+		variant: 'default' | 'secondary' | 'ghost'
+		session: boolean
+	}[] = [
+		{
+			title: 'Login',
+			href: '/login',
+			className: '',
+			variant: 'ghost',
+			session: false
+		},
+		{
+			title: 'Signup',
+			href: '/signup',
+			className: '',
+			variant: 'default',
+			session: !!session
+		}
+	]
+
 	return (
 		<NavigationMenu className='ml-auto'>
 			<NavigationMenuList className='flex items-center gap-4'>
-				{navigationList.map((navigation, i) => (
-					<NavigationMenuItem key={i}>
-						<Button variant='default' className='hover:text-white' asChild>
-							<Link href={navigation.href}>{navigation.title}</Link>
-						</Button>
-					</NavigationMenuItem>
-				))}
+				{navigationList
+					.filter((navigation) => !navigation.session)
+					.map((navigation, i) => (
+						<NavigationMenuItem key={i}>
+							<Button
+								variant={navigation.variant}
+								className={navigation.className}
+								asChild
+							>
+								<Link href={navigation.href}>{navigation.title}</Link>
+							</Button>
+						</NavigationMenuItem>
+					))}
 			</NavigationMenuList>
 		</NavigationMenu>
 	)
