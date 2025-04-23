379879f4df46602dd1d9f78f2fb90e9951f3516c
maeta187 <nexus18789@gmail.com>
2025-04-23 21:27:23 +0900

    ✨ : InputFormの実装

---
+++ src/app/login/_components/InputForm.tsx
@@ -0,0 +1,46 @@
+import { InputElement } from '@/components/common/FormElements'
+import { Button } from '@/components/ui/button'
+import { Form } from '@/components/ui/form'
+import { LoginFormType } from '@/types'
+import { SubmitHandler, UseFormReturn } from 'react-hook-form'
+
+type InputFormProps = {
+	form: UseFormReturn<LoginFormType>
+	onSubmit: SubmitHandler<LoginFormType>
+}
+
+export const InputForm = ({ form, onSubmit }: InputFormProps) => {
+	const {
+		handleSubmit,
+		control,
+		formState: { isSubmitting }
+	} = form
+
+	return (
+		<Form {...form}>
+			<form onSubmit={handleSubmit(onSubmit)} className='space-y-8'>
+				<InputElement
+					label='メールアドレス'
+					name='email'
+					type='email'
+					placeholder='example@example.com'
+					control={control}
+				/>
+
+				<InputElement
+					label='パスワード'
+					name='password'
+					type='password'
+					placeholder='••••••••'
+					control={control}
+				/>
+
+				<div className='flex justify-end gap-4'>
+					<Button type='submit' size='lg' disabled={isSubmitting}>
+						{isSubmitting ? 'ログイン中...' : 'ログイン'}
+					</Button>
+				</div>
+			</form>
+		</Form>
+	)
+}
