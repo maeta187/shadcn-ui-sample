0a25999cb538f4b1a98308b67e6395cddace3948
maeta187 <nexus18789@gmail.com>
2025-05-05 10:27:02 +0900

    💄 : Signupフォームをカードコンテンツを使用して構成するよう変更

--- src/app/signup/_components/InputForm.tsx
+++ src/app/signup/_components/InputForm.tsx
@@ -3,17 +3,13 @@ import {
 	RadioElement,
 	SelectElement
 } from '@/components/common/FormElements'
-import { Button } from '@/components/ui/button'
-import { Form } from '@/components/ui/form'
 import { GENDER } from '@/constants'
 import { PrefectureOptions, SignupFormType } from '@/types'
-import { SubmitHandler, UseFormReturn } from 'react-hook-form'
+import { Control } from 'react-hook-form'
 
 interface InputFormProps {
-	form: UseFormReturn<SignupFormType>
-	onSubmit: SubmitHandler<SignupFormType>
 	prefectureOptions: PrefectureOptions[]
-	handleReset: () => void
+	control: Control<SignupFormType>
 }
 
 const genderOptions = [
@@ -22,90 +18,69 @@ const genderOptions = [
 	{ value: GENDER.OTHER, label: 'その他' }
 ]
 
-export const InputForm = ({
-	form,
-	onSubmit,
-	prefectureOptions,
-	handleReset
-}: InputFormProps) => {
-	const {
-		handleSubmit,
-		control,
-		formState: { isSubmitting }
-	} = form
-
+export const InputForm = ({ prefectureOptions, control }: InputFormProps) => {
 	return (
-		<Form {...form}>
-			<form onSubmit={handleSubmit(onSubmit)} className='space-y-8'>
-				<InputElement
-					label='お名前'
-					name='userName'
-					placeholder='山田太郎'
-					control={control}
-				/>
+		<>
+			<InputElement
+				label='お名前'
+				name='userName'
+				placeholder='山田太郎'
+				control={control}
+			/>
 
-				<InputElement
-					label='フリガナ'
-					name='userNameKana'
-					placeholder='ヤマダタロウ'
-					control={control}
-				/>
+			<InputElement
+				label='フリガナ'
+				name='userNameKana'
+				placeholder='ヤマダタロウ'
+				control={control}
+			/>
 
-				<RadioElement
-					label='性別'
-					name='gender'
-					control={control}
-					options={genderOptions}
-				/>
+			<RadioElement
+				label='性別'
+				name='gender'
+				control={control}
+				options={genderOptions}
+			/>
 
-				<InputElement
-					label='電話番号'
-					name='phoneNumber'
-					type='tel'
-					placeholder='09012345678'
-					control={control}
-				/>
+			<InputElement
+				label='電話番号'
+				name='phoneNumber'
+				type='tel'
+				placeholder='09012345678'
+				control={control}
+			/>
 
-				<InputElement
-					label='メールアドレス'
-					name='email'
-					type='email'
-					placeholder='example@example.com'
-					control={control}
-				/>
+			<InputElement
+				label='メールアドレス'
+				name='email'
+				type='email'
+				placeholder='example@example.com'
+				control={control}
+			/>
 
-				<SelectElement
-					label='都道府県'
-					name='prefecture'
-					options={prefectureOptions}
-					placeholder='都道府県を選択してください'
-					control={control}
-				/>
+			<SelectElement
+				label='都道府県'
+				name='prefecture'
+				options={prefectureOptions}
+				placeholder='都道府県を選択してください'
+				control={control}
+			/>
 
-				<InputElement
-					label='パスワード'
-					name='password'
-					type='password'
-					placeholder='••••••••'
-					control={control}
-				/>
+			<InputElement
+				label='パスワード'
+				name='password'
+				type='password'
+				placeholder='••••••••'
+				control={control}
+			/>
 
-				<InputElement
-					label='パスワード（確認）'
-					name='confirmPassword'
-					type='password'
-					placeholder='••••••••'
-					control={control}
-				/>
-				<div className='flex justify-end gap-4'>
-					<Button type='button' variant='outline' onClick={handleReset}>
-						リセット
-					</Button>
-					<Button type='submit' size='lg' disabled={isSubmitting}>
-						{isSubmitting ? '送信中...' : '登録'}
-					</Button>
-				</div>
-			</form>
-		</Form>
+			<InputElement
+				label='パスワード（確認）'
+				name='confirmPassword'
+				type='password'
+				placeholder='••••••••'
+				control={control}
+			/>
+		</>
 	)
 }
